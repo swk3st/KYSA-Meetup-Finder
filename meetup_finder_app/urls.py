1791106a49bab05be51595709a6b60752c472ba9
givenaj <49413914+givenaj@users.noreply.github.com>
2020-10-30 16:08:46 -0400

    linked pages together

--- meetup_finder_app/urls.py
+++ meetup_finder_app/urls.py
@@ -3,16 +3,18 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
+from django.urls import reverse
+from django.views import generic
 from django.conf.urls import include
 
 
 app_name = 'meetup_finder_app'
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
-    path('newevent/', TemplateView.as_view(template_name="meetup_finder_app/newEvent.html")),
+    path('newevent/', TemplateView.as_view(template_name="meetup_finder_app/newEvent.html"), name="newevent"),
     path('createEvent/', views.createEvent, name = 'createEvent'),
     path('accounts/profile/', views.WelcomeView),
-    path('upcoming/', views.UpcomingView.as_view()),
+    path('upcoming/', views.UpcomingView.as_view(), name="upcoming"),
     path('<int:pk>', views.DetailView.as_view(), name = "detail"),
     path('', views.HomeView.as_view(), name='home'),
 ]

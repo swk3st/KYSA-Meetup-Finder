a55bed7e44a8dbd284b3c849701a25cd758031c9
givenaj <49413914+givenaj@users.noreply.github.com>
2020-11-24 14:50:13 -0500

    writing tests

--- meetup_finder_app/tests.py
+++ meetup_finder_app/tests.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
-from .models import Event
+from .models import Event, AppUser
+from .views import requestFriend
 import datetime
 from allauth.account.forms import LoginForm
 class YourLoginForm(LoginForm):
@@ -28,7 +30,7 @@ class YourLoginForm(LoginForm):
             ),
         )
         self.helper = helper
-def create_event(organizer, desctiption, location, days):
+def create_event(organizer, description, location, days):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
@@ -38,6 +40,10 @@ def create_event(organizer, desctiption, location, days):
     return Event.objects.create(event_organizer=organizer, event_desctiption=description, event_location=loaction, event_date = time)
 
 class EventModelTests(TestCase):
+
+    def test_interested_user(self):
+        """
+        """
 
     def test_coming_up_one_day_until(self):
         """
@@ -64,3 +70,33 @@ class EventModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=10)
         event = Event(event_date=time)
         self.assertIs(event.coming_up(), False)
+
+
+
+class AppUserTests(TestCase):
+
+    def test_requestFriend(self):
+        """
+        requestFriend should add a user to another's requested friends
+        """
+        u = AppUser(id = 1)
+        u.django_user = User.objects.create(username='Testuser')
+        u.save()
+        f = AppUser(id = 2)
+        f.django_user = User.objects.create(username='Testuser2')
+        f.save()
+        c = Client()
+        c.post("/requestFriend",{'Friend':f.id,'User':u.id})
+        client = Client()
+        response = client.get("/requestFriend")
+        request = response.wsgi_request 
+        #request.POST({'Friend':f.id,'User':u.id})
+        #response = self.client.get(reverse('meetup_finder_app:requestFriend'))
+        #f.requested_friends.add(u)
+        requestFriend(request)
+
+        self.assertIs(u in f.requested_friends.all(), True)
+    
+
+
+
